3211a1b760aff7a54788b67ea8206cb93819f9cd
jacek <jacek@jacek>
2023-03-31 15:54:34 +0200

    Removed footer

--- footer.php
+++
@@ -1,6 +0,0 @@
-<?php declare(strict_types=1);  /* Ta linia musi być pierwsza */ ?>
-	<footer class="page-footer mt-auto bg-light border-primary">   
-		<div class="container-fluid">
-			Zawartość Twojego footera
-		</div>
-	</footer>	
